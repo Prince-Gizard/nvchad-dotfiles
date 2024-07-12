587925de80d48d3214f6f6c3f6a04200b1f5c70b
BuraevVV <BuraevVV@mosreg.ru>
2024-07-12 22:43:04 +0300

    scod
    commit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -215,27 +215,24 @@ return {
   }, -- To make a plugin not be loaded
   {
     "luckasRanarison/tailwind-tools.nvim",
+    lazy = false,
     dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      require("tailwind-tools").setup {
-        opts = {
-          document_color = {
-            enabled = true, -- can be toggled by commands
-            kind = "inline", -- "inline" | "foreground" | "background"
-            inline_symbol = "󰝤 ", -- only used in inline mode
-            debounce = 200, -- in milliseconds, only applied in insert mode
-          },
-          conceal = {
-            enabled = false, -- can be toggled by commands
-            min_length = nil, -- only conceal classes exceeding the provided length
-            symbol = "󱏿", -- only a single character is allowed
-            highlight = { -- extmark highlight options, see :h 'highlight'
-              fg = "#38BDF8",
-            },
-          },
-          custom_filetypes = {},
+    opts = {
+      document_color = {
+        enabled = true, -- can be toggled by commands
+        kind = "inline", -- "inline" | "foreground" | "background"
+        inline_symbol = "󰝤 ", -- only used in inline mode
+        debounce = 200, -- in milliseconds, only applied in insert mode
+      },
+      conceal = {
+        enabled = false, -- can be toggled by commands
+        min_length = nil, -- only conceal classes exceeding the provided length
+        symbol = "󱏿", -- only a single character is allowed
+        highlight = { -- extmark highlight options, see :h 'highlight'
+          fg = "#38BDF8",
         },
-      }
-    end,
+      },
+      custom_filetypes = {}, -- see the extension section to learn how it works
+    },
   },
 }
